60cce7d72abb0b779288ded1be3188f38cb4dd17
bfloriano <b.floriano@outlook.com>
2024-02-23 15:07:37 -0300

    feat: create controllers and DTOs

--- src/main/kotlin/me/dio/creditapplicationsystem/entity/Credit.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/entity/Credit.kt
@@ -13,7 +13,7 @@ data class Credit(
         @Column(nullable = false) val creditValue: BigDecimal = BigDecimal.ZERO,
         @Column(nullable = false) val dayFirstInstallment: LocalDate,
         @Column(nullable = false) val numberOfInstallments: Int = 0,
-        @Enumerated val status: Status,
+        @Enumerated val status: Status = Status.IN_PROGRESS,
         @ManyToOne var customer: Customer? = null,
         @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null
 )
